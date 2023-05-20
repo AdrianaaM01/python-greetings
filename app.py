ad18c5b7d121c6eec37842947c5a33b6228b9ce0
AdrianaaM01 <adriana.markevica@inbox.lv>
2023-05-20 11:31:50 +0300

    port probs

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def greetings():
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--port', default=5001, type=int, help='Port to run the server on.')
+    parser.add_argument('--port', default=5000, type=int, help='Port to run the server on.')
     args = parser.parse_args()
     
-    app.run(debug=False, port=args.port)
+    app.run(debug=True, host='localhost', port=args.port)
